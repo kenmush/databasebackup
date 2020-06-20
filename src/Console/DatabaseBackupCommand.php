409865c94285bbeac439ced2a492910c979be9a8
Kennedy Mutisya <ken.msh@gmail.com>
2020-06-20 23:37:08 +0300

    Backup command update

--- src/Console/DatabaseBackupCommand.php
+++ src/Console/DatabaseBackupCommand.php
@@ -5,6 +5,7 @@ namespace Mutisya\DatabaseBackup\Console;
 
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 use Mutisya\DatabaseBackup\Facades\Backup;
 
 class DatabaseBackupCommand extends Command
@@ -15,6 +16,10 @@ class DatabaseBackupCommand extends Command
 
     public function handle()
     {
-     $this->info((new \Mutisya\DatabaseBackup\DatabaseBackup())->backup());
+        $filename = "backup-" . Carbon::now()->format('Y-m-d') . ".gz";
+        $command = "mysqldump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | gzip > " . storage_path() . "/app/backup/" . $filename;
+        $returnVar = NULL;
+        $output  = NULL;
+        exec($command, $output, $returnVar);
     }
 }
